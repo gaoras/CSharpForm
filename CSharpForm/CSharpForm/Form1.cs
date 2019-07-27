cfdd51b11ff4986b115d7a3e9bc1cd0eb0f7343b
Yu <imok221@yahoo.co.jp>
2019-07-27 18:57:14 +0900

    追加できない問題を解決

---
+++ CSharpForm/CSharpForm/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibFileManagement;
+
+namespace CSharpForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Disp_Click(object sender, EventArgs e)
+        {
+            DataGridViewManagement DGVMng = new DataGridViewManagement();
+            
+
+        }
+    }
+}
