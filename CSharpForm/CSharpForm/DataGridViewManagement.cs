7663b8ca89cdb95ccf5351c63216832a42574833
Yu <imok221@yahoo.co.jp>
2019-07-27 19:18:22 +0900

    DataGridViewManagementを更新

--- CSharpForm/CSharpForm/DataGridViewManagement.cs
+++ CSharpForm/CSharpForm/DataGridViewManagement.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CSharpForm
 {
     class DataGridViewManagement
     {
+        private DataGridView DataGridView;
 
+        public DataGridViewManagement(DataGridView dataGridView1)
+        {
+            this.DataGridView = dataGridView1;
+        }
+
+        /// <summary>
+        /// 指定したデータをデータグリッドビューに表示
+        /// </summary>
+        /// <param name="csvData"></param>
+        public void Disp(string[][] csvData)
+        {
+            //TODO: データ表示
+            
+        }
     }
 }
--- CSharpForm/CSharpForm/Form1.cs
+++ CSharpForm/CSharpForm/Form1.cs
@@ -20,9 +20,13 @@ namespace CSharpForm
 
         private void button_Disp_Click(object sender, EventArgs e)
         {
-            DataGridViewManagement DGVMng = new DataGridViewManagement();
-            
+            DataGridViewManagement DGVMng = new DataGridViewManagement(dataGridView1);
 
+            //csvファイルの読み込み
+            string path = "";
+            string[][] csvData = CsvReader.Read(path);
+            //データグリッドビューに表示
+            DGVMng.Disp(csvData);
         }
     }
 }
